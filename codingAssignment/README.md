20d66abb0e6788486f9413c9629c6f952fa9fd1e
jithin p nair <jithinpnair@gmail.com>
2021-04-25 21:48:35 +0530

    search image and add to favourite album

--- codingAssignment/README.md
+++ codingAssignment/README.md
@@ -1,11 +1,8 @@
-# coding-assignment
-coding assignment for image search
+# Assignment
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.4.
 
 ## Development server
-
-Run `npm install` to install all the dependencies.
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
@@ -25,3 +22,7 @@ Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.
 
 Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
 
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+# girmiti-verifone-assignment
---
+++ codingAssignment/src/app/constant/resource.constant.ts
@@ -0,0 +1,78 @@
+export class ResourceConstants {
+
+  static RESOURCES = {
+    'nav.searchKey.placeholder.value': 'Search Images',
+    'showImage.image.not.found.message': 'No records found',
+    'search.image.pageSize': 10,
+    'search.image.pageIndex': [10, 20, 30],
+    'image.search.url': 'https://api.unsplash.com/search/photos?query=0&orientation=landscape&per_page=1&page=2',
+    'nav.Favorites.dropdown': 'My Favorites',
+    'search.image.init,searchKey': 'Forest',
+
+
+    // Add to collection popup resources ;
+    'addcollectionpopup.close.icon': 'close',
+    'addcollectionpopup.add.icon': 'add',
+    'addcollectionpopup.remove.icon': 'remove',
+    'addcollectionpopup.heading.label': 'Add to My Album',
+    'addcollectionpopup.buttontext.text': 'Create new album',
+    'addcollectionpopup.imageadd.text': '0 photos',
+    'addcollectionpopup.imageremove.text': '1 photos',
+    'addcollectionpopup.Collectionname.text': 'Album Name',
+    'addcollectionpopup.Description.text': 'Description (Optional)',
+    'addcollectionpopup.button.Createcollection': 'Create Album',
+    'addcollectionpopup.button.Cancel': 'Cancel',
+    'addcollectionpopup.create.collection.heading.label': 'Create New Album',
+    'total.phots.label': 'Total Images : ',
+
+    // Favourite & collection resource Constant;
+    'favourite.label.name': 'Album',
+    'favourite.label.number': '(3)',
+    'favourite.collection1.label': 'Album 1',
+    'favourite.collection2.label': 'Album 2',
+    'favourite.collection3.label': 'Album 3',
+    'favourite.author.name': 'Vincent Hawks',
+    'favourite.picture.type1': 'Nature',
+    'favourite.picture.type2': 'Morning',
+    'favourite.picture.type3': 'Animal',
+    'favourite.picture.type4': 'Scenery',
+    'favourite.picture.type5': 'Forest',
+    'favourite.picture.type6': 'Natural Beauty',
+    'favourite.collection.description.name': '6 photos.Curated by ',
+    'favourite.imgCollection.description': 'Description',
+    'favourite.imgCollection.description1': 'Description of Album 1',
+    'favourite.imgCollection.description2': 'Description of Album 2',
+    'favourite.imgCollection.description3': 'Description of Album 3',
+    'favourite.button.name.edit': 'Edit',
+    'favourite.button.name.share': 'Share',
+    'favourite.edit.label': 'Edit Album',
+    'favourite.input.label.name': 'Name',
+    'favourite.checkbox.label.name': 'Make Album Private',
+    'favourite.anchor.label.name': 'Delete Album',
+    'favourite.button.label.save': 'Save',
+    'favourite.label.photoNum': '5 Photos',
+    'favourite.input.length': '60',
+    'favourite.textarea.length': '250',
+    'addToFavourite.alertMsg': 'Sorry, a Album already exists with this name - ',
+
+    // EDITCOLLECTION CONSTANTS
+    'editcollections.maticon.close': 'close',
+    'editcollections.heading.label': 'Edit Album',
+    'editcollections.name.label': 'Name',
+    'editcollections.description.label': 'Description',
+    'editcollections.delete.button': 'Delete',
+    'editcollections.updatename.close': 'Update Name',
+    'editcollections.alertMsg': 'Album Name already exits with the name - ',
+
+    // MANAGECOLLECTION CONSTANTS
+    'manageCollections.head.collectionName': 'Album Name',
+    'manageCollections.para': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, harum.',
+    'manageCollections.image.name': 'Edit Album',
+    'name.already exist.lable': 'Album Name already exists with the name - ',
+    'ok.button.lable': 'Ok'
+
+
+
+
+  };
+}
